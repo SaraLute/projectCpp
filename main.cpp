c81f8ad610c2bf387b8735fa8bef36f373a722a9
Sara Lute <saralute@Saras-MBP.home>
2020-11-01 22:12:18 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include "newUser.h"
+
+// This is the main script. When running this, directly or indirectly all functions and other scripts are used.
+// Starting with setupBook to setup the maps and addressBook.
+
+int main() {
+    // setupBook
+    std::cout << "Hello, and welcome to this food ordering system." << std::endl;
+    std::cout << "Do you have an account? [Y|N] " << std::endl;
+    char action;
+    std::cin >> action;
+
+    // The action stored in 'action' is used in this switch to either make an account first or to go directly
+    // to placing the order. After the account is setup with the order attached, it is send on to pick the nearest
+    // deliveryman and then into the TrackDelivery script.
+
+    switch (action) {
+        case 'N' :
+            //newUser
+
+            std::cout << "Thanks for signing up!" << std::endl;
+            std::cout << "Do you want to order now? [Y|N] " << std::endl;
+            char action2;
+            std::cin >> action2;
+            switch (action2) {
+                case 'Y' :
+                    std::cout << "Then let's go get your order!" << std::endl;
+                    //getOrder
+                    //findAccount
+                    //deliveryMan
+                    // TrackDelivery
+                    break;
+                case 'N' :
+                    std::cout << "Thanks for visiting the system, goodbye!" << std::endl;
+                    break;
+                default:
+                    std::cout << "This is not an option..." << std::endl;
+            }
+            break;
+        case 'Y' :
+            std::cout << "Then let's go get your order!" << std::endl;
+            //getOrder
+            //findAccount
+            //deliveryMan
+            //trackDelivery
+            break;
+        default:
+            std::cout << "This is not an option..." << std::endl;
+    }
+    return 0;
+}
